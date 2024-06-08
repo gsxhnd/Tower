31ac180ea3443ccbdf23f732c5bca957b9b7a423
gsxhnd <gsxhnd@gmail.com>
2024-06-08 21:11:03 +0800

    config update

--- utils/config.go
+++ utils/config.go
@@ -1,22 +1,43 @@
 package utils
 
 import (
-	"github.com/caarlos0/env/v8"
+	"os"
+
+	"gopkg.in/yaml.v3"
 )
 
 type Config struct {
-	Dev         bool `env:"DEV" envDefault:"true"`
-	Debug       bool `env:"DEBUG" envDefault:"true"`
-	Port        string
-	LogLevel    string `env:"LOG_LEVEL" envDefault:"info"`
-	TraceEnable bool   `env:"TRACE_ENABLE" envDefault:"false"`
-	TraceUrl    string `env:"TRACE_URL"`
+	Dev         bool   `yaml:"dev"`
+	Port        string `yaml:"port"`
+	WebEnable   bool   `yaml:"web_enable"`
+	TraceEnable bool   `yaml:"trace_enable"`
+	TraceUrl    string
+	LogConfig   LogConfig `yaml:"log,omitempty"`
+	LogLevel    string
 }
 
+type LogConfig struct {
+	Level       string
+	Filename    string `yaml:"filename"`
+	MaxSize     int
+	MaxAge      int
+	MaxBackups  int
+	TraceEnable bool
+	TraceUrl    string
+}
+
+// TODO: need config path
 func NewConfig() (*Config, error) {
-	cfg := Config{}
-	if err := env.Parse(&cfg); err != nil {
+	var cfg = Config{}
+
+	file, err := os.ReadFile("")
+	if err != nil {
 		return nil, err
 	}
+
+	if err := yaml.Unmarshal(file, &cfg); err != nil {
+		return nil, err
+	}
+
 	return &cfg, nil
 }
